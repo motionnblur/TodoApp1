915441926592be59d884cebb3512db8d1713a9e1
chill <chill@mail.com>
2024-06-08 05:35:24 +0300

    fix
    What was the problem ? : Since @RequestBody annotation wasn't used in "addTodo" function, the dto object was being passed as NULL.
    What is the solution ? : Added @RequestBody param to addTodo(<<here>>TodoEntityDto todoEntityDto)

--- src/main/java/com/example/app/dto/TodoEntityDto.java
+++ src/main/java/com/example/app/dto/TodoEntityDto.java
@@ -1,10 +1,8 @@
 package com.example.app.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class TodoEntityDto {
     private String todoName;
     private String todoBody;
