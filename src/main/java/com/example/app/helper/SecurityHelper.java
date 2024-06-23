61deade82af647ac4e9f643b071f71e8e52d4e77
chill <chill@mail.com>
2024-06-23 17:09:45 +0300

    Fix StringHelper field in SecurityHelper

--- src/main/java/com/example/app/helper/SecurityHelper.java
+++ src/main/java/com/example/app/helper/SecurityHelper.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 public class SecurityHelper {
-    private final StringHelper stringHelper;
+    private StringHelper stringHelper;
     public SecurityHelper(){
         this.stringHelper = new StringHelper();
     }
